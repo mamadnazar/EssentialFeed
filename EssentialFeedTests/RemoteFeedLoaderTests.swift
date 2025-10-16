7b14ac288c4ddd1740f9b7cc6f2d066e9d19b8aa
mamadnazar <shohkarim7@gmail.com>
2025-10-16 11:38:34 +0500

    Request data from URL on load

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -9,10 +9,16 @@ import Testing
 import Foundation
 
 class RemoteFeedLoader {
-    
+    func load() {
+        HTTPClient.shared.requestedURL = URL(string: "https://a-url.com")
+    }
 }
 
 class HTTPClient {
+    static let shared = HTTPClient()
+    
+    private init () {}
+    
     var requestedURL: URL?
 }
 
@@ -20,10 +26,18 @@ struct RemoteFeedLoaderTests {
 
     @Test func test_init_doesNotRequestDataFromURL() async throws {
         // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-        let client = HTTPClient()
+        let client = HTTPClient.shared
         _ = RemoteFeedLoader()
         
         #expect((client.requestedURL == nil))
     }
 
+    @Test func test_load_requestDataFromURL() async throws {
+        let client = HTTPClient.shared
+        let sut = RemoteFeedLoader()
+        
+        sut.load()
+        
+        #expect(client.requestedURL != nil)
+    }
 }
