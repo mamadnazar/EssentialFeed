48d6f070ce74ef92b623dd8a0993b5fca5aec857
mamadnazar <shohkarim7@gmail.com>
2025-10-16 11:52:49 +0500

    Make HTTP client a protocol

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -9,22 +9,26 @@ import Testing
 import Foundation
 
 class RemoteFeedLoader {
+    let client: HTTPClient
+    
+    init(client: HTTPClient) {
+        self.client = client
+    }
+    
     func load() {
-        HTTPClient.shared.get(from: URL(string: "https://a-url.com")!)
+        client.get(from: URL(string: "https://a-url.com")!)
     }
 }
 
-class HTTPClient {
-    static var shared = HTTPClient()
-    
-    func get(from url: URL) {}
+protocol HTTPClient {
+    func get(from url: URL)
 }
 
 class HTTPClientSpy: HTTPClient {
     
     var requestedURL: URL?
     
-    override func get(from url: URL) {
+    func get(from url: URL) {
         requestedURL = url
     }
 }
@@ -34,16 +38,14 @@ struct RemoteFeedLoaderTests {
     @Test func test_init_doesNotRequestDataFromURL() async throws {
         // Write your test here and use APIs like `#expect(...)` to check expected conditions.
         let client = HTTPClientSpy()
-        HTTPClient.shared = client
-        _ = RemoteFeedLoader()
+        _ = RemoteFeedLoader(client: client)
         
         #expect((client.requestedURL == nil))
     }
 
     @Test func test_load_requestDataFromURL() async throws {
         let client = HTTPClientSpy()
-        HTTPClient.shared = client
-        let sut = RemoteFeedLoader()
+        let sut = RemoteFeedLoader(client: client)
         
         sut.load()
         
